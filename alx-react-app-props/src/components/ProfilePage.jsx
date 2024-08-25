a879456dc82dcb446df5e0646aefc987db0a5fa1
Ojo Funmilayo <ojofayo56@gmail.com>
2024-08-25 00:01:22 -0400

    alot

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return
+ <div>
+   <UserInfo />;
+ </div>
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createContext } from 'react';
+
+function UserContext() {
+    const UserContext = createContext();
+  return (
+    <div>UserContext</div>
+  )
+}
+
+export default UserContext
