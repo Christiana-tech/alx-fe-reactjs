c6f5f5087697e0c2290ab4cf038ede9012e201f4
Ojo Funmilayo <ojofayo56@gmail.com>
2024-09-01 12:14:43 -0400

    commit all changes

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Profile from './components/Profile';
+import BlogPost from './components/BlogPost';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
