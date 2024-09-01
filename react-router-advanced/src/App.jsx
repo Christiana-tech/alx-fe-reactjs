9c9e4a40431876edb682f7dbeba2f97e8f7d9b59
Ojo Funmilayo <ojofayo56@gmail.com>
2024-09-01 12:29:33 -0400

    all

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
 import Profile from './components/Profile';
 import BlogPost from './components/BlogPost';
 
@@ -18,7 +17,7 @@ function App() {
             </ProtectedRoute>
           }
         />
-         <Route path="/blog/:postId" element={<BlogPost />} />
+         <Route path="/blog/:Id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
